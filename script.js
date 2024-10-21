91918ebed413414f3734d54ce49752ff28398914
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2024-10-21 22:44:04 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+// array practice
+
+let friendsList = ['Akash', 'Titu', 'Kamrul', 'Rakib']
+
+console.log(friendsList);
+console.log(friendsList[1]);
+
+// function practice 
+
+function myFirstFunc() {
+  let x = 10
+  let y = 12
+  let z = x*y
+  console.log(z);
+} 
+myFirstFunc()
+
+// object practice
+
+let rakibulIslam = {
+  homeTown: 'Khustia',
+  presentAddress: 'Dhaka'
+}
+
+console.log(rakibulIslam.homeTown);
+
+// object destructuring
+
+let {name, home} = {
+  name: 'Rakibul Islam',
+  home: 'Dhaka'
+}
+
+console.log(name);
+console.log(home);
+
